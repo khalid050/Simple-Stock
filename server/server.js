27965b9aa2fa679c0353d29c79d9ad319f1db6d3
Nathan Koike <nathankoike@Nathans-MacBook-Air.local>
2019-10-05 18:41:06 -0400

    takes command line arguments for python scripts

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const companyRoutes = require("./controllers/companies");
 const fs = require("fs");
-const { spawn } = require("child_process");
+const { spawnSync } = require("child_process");
+const { exec } = require("child_process");
 
 const app = express();
 const port = 3000;
@@ -18,20 +19,32 @@ app.use(
 app.use(express.static("build"));
 
 // run a python script, takes a file name as a parameter
-function runScript(file) {
-  return spawn("python", ["-u", path.join(__dirname, "../python", file)]);
+function runScript(file, args) {
+  // establish the tail of the python script call
+  tail = [path.join(__dirname, "../python", file)];
+
+  // add each argument to the tail
+  args.forEach(item => {
+    tail.push(item);
+  });
+
+  // run the python script
+  return spawnSync("python", tail);
 }
 
 app.get("/companies", (req, res) => {
   // run a python script
-  const subprocess = runScript("test.py");
+  const subprocess = runScript("clTest.py", [2]);
 
   var outputData = null;
 
   // send the data from the standard output of the python script
-  subprocess.stdout.on("data", data => {
-    res.send(data);
-  });
+  res.send(subprocess.stdout);
+
+  // exec(`python ${__dirname}/clTest.py 0`, (err, stdout, stderr) => {
+  //   if (err) res.send(stderr);
+  //   res.send(stdout);
+  // });
 });
 
 // catch all
