634a69b2e837f3f826d07a97c4584d7f855a91e7
Kennard Ezekiel Fung <kenfung@Kennards-MacBook.local>
2019-10-05 16:27:23 -0400

    Added Capital Acquisition and Income Quality into investment JSON

--- python/econstat.py
+++ python/econstat.py
@@ -28,20 +28,16 @@ def loans():
 	pass
 
 def investment(ticker):
-
-
 	#Open up the list of stocks
 	stock_list = list(open("stock_symbol_file", 'r'))
 	actual_stock_list = stock_list[0]
 	# print(type(main_stock_list))
-
 
 	#Check if the stock ticker exists in the overall list, if not return an error.
 	if ticker not in actual_stock_list:
 		return("Stock does not exist in this server!")
 
 	else:
-
 		#First, collect the data for the three main financial statements
 		balance_sheet_base = "https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/"
 		balance_sheet_url = balance_sheet_base + str(ticker)
@@ -60,18 +56,22 @@ def investment(ticker):
 
 		IS_financials = income_statement_json.get("financials")
 		CF_financials = cash_flow_json.get("financials")
+		BS_financials = balance_sheet_json.get("financials")
 
 		#Then, collect the data for the big five criteria
 		#CAGR Revenue
 		#Try getting the CAGR for eight years, if not available, then five, if not, three
 		IS_length = len(IS_financials)
 		last_revenue = float(IS_financials[0].get("Revenue"))
+		last_net_income = float(IS_financials[0].get("Net Income"))
 
 		if IS_length >= 5:
 			first_revenue = float(IS_financials[4].get("Revenue"))
+			first_net_income = float(IS_financials[4].get("Net Income"))
 			year_count = 4
 		else:
 			first_revenue = float(IS_financials[CF_length - 1].get("Revenue"))
+			first_net_income = float(IS_financials[CF_length - 1].get("Net Income"))
 			year_count = CF_length
 
 		CAGR_Revenue = (((last_revenue/first_revenue) ** (1/year_count)) - 1) * 100
@@ -81,6 +81,7 @@ def investment(ticker):
 		CF_length = len(CF_financials)
 		last_capex = float(CF_financials[0].get("Capital Expenditure"))
 		last_dividends = float(CF_financials[0].get("Dividend payments"))
+		last_ops_CF = float(CF_financials[0].get("Operating Cash Flow"))
 
 		if CF_length >= 5:
 			first_capex = float(CF_financials[4].get("Capital Expenditure"))
@@ -91,7 +92,6 @@ def investment(ticker):
 			first_dividends = float(CF_financials[CF_length - 1].get("Dividend payments"))
 			year_count = CF_length
 
-
 		#CAGR Dividends
 		CAGR_Dividends = (((last_dividends/first_dividends) ** (1/year_count)) - 1) * 100
 		CAGR_Dividends_rounded = str(round(CAGR_Dividends, 2)) + "%"
@@ -99,21 +99,58 @@ def investment(ticker):
 		CAGR_Capex = (((last_capex/first_capex) ** (1/year_count)) - 1) * 100
 		CAGR_Capex_rounded = str(round(CAGR_Capex, 2)) + "%"
 
-		return(CAGR_Dividends_rounded, CAGR_Capex_rounded)
+
 
 		#Short-Term Debt Ratio
-        #Grab short term debt and total assets for the latest year
+		#Grab short term debt and total assets for the latest year
 
+		st_debt = float(BS_financials[0].get("Short-term debt"))
+		total_assets = float(BS_financials[0].get("Total assets"))
+
+		st_ratio = (st_debt/total_assets) * 100
+		st_ratio_rounded = str(round(st_ratio, 2)) + "%"
 
 		#Current Ratio
+		current_assets = float(BS_financials[0].get("Total current assets"))
+		current_liabilities = float(BS_financials[0].get("Total current liabilities"))
+
+		current_ratio = (current_assets/current_liabilities) * 100
+		current_ratio_rounded = str(round(current_ratio, 2)) + "%"
 
 		#Capital Acquisition Ratio
+		#divide cash flow from ops by CAPEX
+		cap_acq = (last_ops_CF/abs(last_capex))
+		cap_acq_rounded = str(round(cap_acq, 2))
+
+		if cap_acq >= 1:
+			cap_acq_question = "True"
+		else:
+			cap_acq_question = "False"
+
 
 		#Quality of Income Ratio
+		#divide operating cash flow by net income
+		income_quality = (last_ops_CF/last_net_income)
+		income_quality_rounded = str(round(income_quality, 2))
+
+		if income_quality >= 1:
+			income_quality_question = "True"
+		else:
+			income_quality_question = "False"
+
+
 
 		#Create a JSON file with all the relevant data
+		investment_json = {}
+
+		investment_json["Capital Acquisition Ratio"] = cap_acq_rounded
+		investment_json["Capital Acquisition Question"] = cap_acq_question
+
+		investment_json["Income Quality Ratio"] = income_quality_rounded
+		investment_json["Income Quality Question"] = income_quality_question
 
 
+		return(investment_json)
 
 if __name__ == '__main__':
-	print(investment("AAPL"))
+	print(investment("MSFT"))
