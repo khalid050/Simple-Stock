2fc46f6f667dd9b04adf388e1eacce40c21024e1
Kennard Ezekiel Fung <kenfung@Kennards-MacBook.local>
2019-10-05 21:03:27 -0400

    Fixed the 4th of July problem.

--- python/historical.py
+++ python/historical.py
@@ -61,9 +61,12 @@ def historical_data(ticker, timeframe = '1Y'):
 		for x in range(len(historical)):
 			print(x)
 			if (str(old_year) + "-" + str(old_month_final) + "-" + str(latest_day_final)) == historical[x].get("date"):
-				#print(historical[x].get("close"))
 				specific_area = x
+				date_found = True
 				break
+			else:
+				continue
+
 		#If the old date does not exist, minus one to the latest day
 		#If the day > 31, add one to the old_month
 		#If the month > 12, add one to the old_year
@@ -86,34 +89,14 @@ def historical_data(ticker, timeframe = '1Y'):
 			latest_day_final = latest_day
 
 
+
 	print(specific_area)
 	print(len(historical))
 	for x in range(specific_area, len(historical)):
 		historical_to_push.append(historical[x])
 
 	historical_to_push = json.dumps(historical_to_push)
-			# else:
-			# 	#If the old date does not exist, add one to the latest day
-			# 	#If the day > 31, add one to the old_month
-			# 	#If the month > 12, add one to the old_year
-			# 	#Go back up to the top
-			# 	latest_day += 1
-			# 	if latest_day > 31:
-			# 		old_month = old_month + 1
-			# 	if old_month > 12:
-			# 		old_year = old_year + 1
-			#
-			# 	#put a 0 in front of the month or the day if they are less than 10
-			# 	if old_month < 10:
-			# 		old_month_final = str("0") + str(old_month)
-			# 	else:
-			# 		old_month_final = old_month
-			#
-			# 	if latest_day < 10:
-			# 		latest_day_final = str("0") + str(latest_day)
-			# 	else:
-			# 		latest_day_final = latest_day
 
-	return(historical_to_push, len(historical_to_push))
+	return(historical_to_push)
 
-print(historical_data("AAPL"))
+print(historical_data("MSFT"))
